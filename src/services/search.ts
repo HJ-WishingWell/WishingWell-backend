b4477ef8fa33be5b38292bec675d08fdff4b13ec
Supanut1911 <ynotmela@gmail.com>
2024-07-23 17:26:17 +0700

    try switch to similaritySearch to use retriever

--- src/services/search.ts
+++ src/services/search.ts
@@ -30,20 +30,31 @@ export const searchSimilarProducts =  async(query: any, maxPrice: number, catego
           embeddingKey: "embedding", // The name of the collection field containing the embedded text. Defaults to "embedding"
         }
       );
-      console.log({
-        query,
-        maxPrice,
-        category
-      });
-      
-      const result = await vectorStore.similaritySearch(query, 10
+      const retrieverOutput = await vectorStore.similaritySearch(query, 10
       , {
         preFilter: {
-          price: { $lte: maxPrice },
-          category: category
+          // price: { $lte: maxPrice },
+          // category: category
         }})
-      console.log(result);
-      return result;
+      console.log(retrieverOutput);
+
+      //MMR
+      // const retriever = await vectorStore.asRetriever({
+      //   k: 10,
+      //   searchType: "mmr",
+      //   searchKwargs: {
+      //     fetchK: 20,
+      //     lambda: 0.1,
+      //   },
+      // });
+      
+      // const retrieverOutput = await retriever.invoke(query);
+      
+      // console.log(retrieverOutput);
+      
+
+
+      return retrieverOutput;
   } catch (error) {
       console.log(error);
   } 
@@ -78,24 +89,24 @@ export const searchQuery = async(query: string, maxPrice: number ,category: stri
 
 //     const resultOne = await vectorStore.similaritySearch("I but new personal computer, so I want to decorate my desk ", 1);
 //     // const resultOne = await vectorStore.similaritySearch("I got some field about 1 acre so I want to make it to be a cactus garden", 1);
-//     console.log(resultOne);
+    // console.log(resultOne);
     
-//     // const resultOne = await vectorStore.maxMarginalRelevanceSearch("I got some field about 1 acre so I want to make it to be a garden with cactus", {
-//     //   k: 10,
-//     //   fetchK: 20, // The number of documents to return on initial fetch
-//     // });
-//     // console.log(resultOne);
+    // const resultOne = await vectorStore.maxMarginalRelevanceSearch("I got some field about 1 acre so I want to make it to be a garden with cactus", {
+    //   k: 10,
+    //   fetchK: 20, // The number of documents to return on initial fetch
+    // });
+    // console.log(resultOne);
 
-//     // Using MMR in a vector store retriever
-//     // const retriever = await vectorStore.asRetriever({
-//     //   searchType: "mmr",
-//     //   searchKwargs: {
-//     //     fetchK: 20,
-//     //     lambda: 0.1,
-//     //   },
-//     // });
-//     // const retrieverOutput = await retriever.invoke("I got some field about 1 acre so I want to make it to be a garden with cactus");
-//     // console.log(retrieverOutput);
-//     return resultOne
-// }
+    // Using MMR in a vector store retriever
+    // const retriever = await vectorStore.asRetriever({
+    //   searchType: "mmr",
+    //   searchKwargs: {
+    //     fetchK: 20,
+    //     lambda: 0.1,
+    //   },
+    // });
+    // const retrieverOutput = await retriever.invoke("I got some field about 1 acre so I want to make it to be a garden with cactus");
+    // console.log(retrieverOutput);
+    // return resultOne
+
 
