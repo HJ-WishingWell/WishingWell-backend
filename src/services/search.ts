4a4fde8aadbd7edc5c2d70ec528bbc0cc4a2b45d
Supanut1911 <ynotmela@gmail.com>
2024-07-24 15:34:10 +0700

    comment unuse code

--- src/services/search.ts
+++ src/services/search.ts
@@ -5,7 +5,7 @@ import { embedText } from "./vectorize";
 var vector_penalty = 1;
 var full_text_penalty = 10;
 
-export const searchSimilarProducts =  async(query: any, maxPrice: number, category: string) => {
+export const hybridSearchProduct =  async(query: any, maxPrice: number, category: string) => {
   try {
     const queryVector = await embedText(query);
     const agg = [
@@ -182,53 +182,54 @@ export const searchSimilarProducts =  async(query: any, maxPrice: number, catego
   } 
 }
 
-export const searchQuery = async(query: string, maxPrice: number ,category: string) => {
-    const results = await searchSimilarProducts(query, maxPrice, category);
 
-    console.log('ผลลัพธ์ที่ค้นหาได้:');
+export const searchQuery = async(query: string, maxPrice: number ,category: string) => {
+    const results = await hybridSearchProduct(query, maxPrice, category);
     results?.forEach(result => {
         console.log(result);
     });
     return results ?? [];
 }
 
-// export const maxiMalmarginalRelevanceSearch = async() => {
-//     const client = new MongoClient(process.env.MONGO_ATLAS_URL || "");
-//     const namespace = "mod_hackathon_db.prodcut_dbs_ao";
-//     const [dbName, collectionName] = namespace.split(".");
-//     const collection = client.db(dbName).collection(collectionName);
+// // //
+// // // export const maxiMalmarginalRelevanceSearch = async() => {
+// // //     const client = new MongoClient(process.env.MONGO_ATLAS_URL || "");
+// // //     const namespace = "mod_hackathon_db.prodcut_dbs_ao";
+// //     const [dbName, collectionName] = namespace.split(".");
+// //     const collection = client.db(dbName).collection(collectionName);
     
-//     //vectorstore
-//     const vectorStore = new MongoDBAtlasVectorSearch(
-//       new CohereEmbeddings({ model: "embed-english-v3.0" }),
-//       {
-//         collection,
-//         indexName: "vsearch_index", // The name of the Atlas search index. Defaults to "default"
-//         textKey: "detail", // The name of the collection field containing the raw content. Defaults to "text"
-//         embeddingKey: "embedding", // The name of the collection field containing the embedded text. Defaults to "embedding"
-//       }
-//     );
+// //     //vectorstore
+// //     const vectorStore = new MongoDBAtlasVectorSearch(
+// //       new CohereEmbeddings({ model: "embed-english-v3.0" }),
+// //       {
+// //         collection,
+// //         indexName: "vsearch_index", // The name of the Atlas search index. Defaults to "default"
+// //         textKey: "detail", // The name of the collection field containing the raw content. Defaults to "text"
+// //         embeddingKey: "embedding", // The name of the collection field containing the embedded text. Defaults to "embedding"
+// //       }
+// //     );
 
-//     const resultOne = await vectorStore.similaritySearch("I but new personal computer, so I want to decorate my desk ", 1);
-//     // const resultOne = await vectorStore.similaritySearch("I got some field about 1 acre so I want to make it to be a cactus garden", 1);
-    // console.log(resultOne);
+// //     const resultOne = await vectorStore.similaritySearch("I but new personal computer, so I want to decorate my desk ", 1);
+// //     // const resultOne = await vectorStore.similaritySearch("I got some field about 1 acre so I want to make it to be a cactus garden", 1);
+// //     console.log(resultOne);
     
-    // const resultOne = await vectorStore.maxMarginalRelevanceSearch("I got some field about 1 acre so I want to make it to be a garden with cactus", {
-    //   k: 10,
-    //   fetchK: 20, // The number of documents to return on initial fetch
-    // });
-    // console.log(resultOne);
+// //     const resultOne = await vectorStore.maxMarginalRelevanceSearch("I got some field about 1 acre so I want to make it to be a garden with cactus", {
+// //       k: 10,
+// //       fetchK: 20, // The number of documents to return on initial fetch
+// //     });
+// //     console.log(resultOne);
 
-    // Using MMR in a vector store retriever
-    // const retriever = await vectorStore.asRetriever({
-    //   searchType: "mmr",
-    //   searchKwargs: {
-    //     fetchK: 20,
-    //     lambda: 0.1,
-    //   },
-    // });
-    // const retrieverOutput = await retriever.invoke("I got some field about 1 acre so I want to make it to be a garden with cactus");
-    // console.log(retrieverOutput);
-    // return resultOne
-
+// //     Using MMR in a vector store retriever
+// //     const retriever = await vectorStore.asRetriever({
+// //       searchType: "mmr",
+// //       searchKwargs: {
+// //         fetchK: 20,
+// //
+//          lambda: 0.1,
+// //       },
+// //     });
+// //     const retrieverOutput = await retriever.invoke("I got some field about 1 acre so I want to make it to be a garden with cactus");
+// //     console.log(retrieverOutput);
+// //     return resultOne
+// //
 
--- src/services/vectorize.ts
+++ src/services/vectorize.ts
@@ -73,9 +73,9 @@ export const  embeddingProduct = async () => {
             new OpenAIEmbeddings({ model: "text-embedding-ada-002", apiKey: process.env.OPENAI_API_KEY }),
             {
                 collection,
-                indexName: "v2search_index", // The name of the Atlas search index. Defaults to "default"
-                textKey: "detail", // The name of the collection field containing the raw content. Defaults to "text"
-                embeddingKey: "embedding", // The name of the collection field containing the embedded text. Defaults to "embedding"
+                indexName: "v2search_index", 
+                textKey: "detail", 
+                embeddingKey: "embedding", 
             }
         )      
 
@@ -86,45 +86,44 @@ export const  embeddingProduct = async () => {
 
 
 
-export const createVectorStore = async() => {
-    const client = new MongoClient(process.env.MONGO_ATLAS_URL || "");
-    const namespace = "mod_hackathon_db.prodcut_dbs_aos";
-    const [dbName, collectionName] = namespace.split(".");
-    const collection = client.db(dbName).collection(collectionName);
+// // export const createVectorStore = async() => {
+// //     const client = new MongoClient(process.env.MONGO_ATLAS_URL || "");
+// //     const namespace = "mod_hackathon_db.prodcut_dbs_aos";
+// //     const [dbName, collectionName] = namespace.split(".");
+// //     const collection = client.db(dbName).collection(collectionName);
 
-    const getproducts = await getallProduct()
-    const products = getproducts?.map((product) => {
-        return `{ name: '${product.name}', detail: '${product.detail}', category: '${product.category}'}`
-    })
-    const productsId = getproducts?.map((product) => {
-        return {
-            id: product._id.toString()
-        }
-    })
+// //     const getproducts = await getallProduct()
+// //     const products = getproducts?.map((product) => {
+// //         return `{ name: '${product.name}', detail: '${product.detail}', category: '${product.category}'}`
+// //     })
+// //     const productsId = getproducts?.map((product) => {
+// //         return {
+// //             id: product._id.toString()
+// //         }
+// //     })
 
-    
+//     /
+// //     //vectorstore
+// //     const vectorStore = await MongoDBAtlasVectorSearch.fromTexts(
+// //         [
+// //             `{"name":"christmas lamp","detail":"christmas lamp for decoration your desk","category":"decorate compute desk"}`,
+// //             `{"name":"Wooden Bookshelf","detail":"White wooden bookshlf, Brown wooden bookshelf, Black wooden bookshelf","category":"decorate compute desk"}`,
+// //             `{"name":"Monstera plastic plant","detail":"Fake plant, plastic plant","category":"decorate compute desk"}`,
+// //             `{"name":"Agricultural fertilizer","detail":"fertilizer for growing plants","category":"house and graden"}`,
+// //             `{"name":"Mini Cactus","detail":"Cactus size mini from India","category":"house and graden"}`,
+// //             `{"name":"Plant shovel","detail":"shovel tool for plant ","category":"house and graden"}`
+// //         ],
+// //         // [{ id: '669e16ed024d527082dd4598' }, {id: '669e1f190d929875d0a2127c'}],
+// //         productsId as any,
+// //         new CohereEmbeddings({ model: "embed-english-v3.0", apiKey: process.env.COHERE_API_KEY || "" }),
+// //     {
+// //         collection,
+// //         indexName: "vsearch_index", // The name of the Atlas search index. Defaults to "default"
+// //         textKey: "detail", // The name of the collection field containing the raw content. Defaults to "text"
+// //         embeddingKey: "embedding", // The name of the collection field containing the embedded text. Defaults to "embedding"
+// //     })
 
-    //vectorstore
-    const vectorStore = await MongoDBAtlasVectorSearch.fromTexts(
-        [
-            `{"name":"christmas lamp","detail":"christmas lamp for decoration your desk","category":"decorate compute desk"}`,
-            `{"name":"Wooden Bookshelf","detail":"White wooden bookshlf, Brown wooden bookshelf, Black wooden bookshelf","category":"decorate compute desk"}`,
-            `{"name":"Monstera plastic plant","detail":"Fake plant, plastic plant","category":"decorate compute desk"}`,
-            `{"name":"Agricultural fertilizer","detail":"fertilizer for growing plants","category":"house and graden"}`,
-            `{"name":"Mini Cactus","detail":"Cactus size mini from India","category":"house and graden"}`,
-            `{"name":"Plant shovel","detail":"shovel tool for plant ","category":"house and graden"}`
-        ],
-        // [{ id: '669e16ed024d527082dd4598' }, {id: '669e1f190d929875d0a2127c'}],
-        productsId as any,
-        new CohereEmbeddings({ model: "embed-english-v3.0", apiKey: process.env.COHERE_API_KEY || "" }),
-    {
-        collection,
-        indexName: "vsearch_index", // The name of the Atlas search index. Defaults to "default"
-        textKey: "detail", // The name of the collection field containing the raw content. Defaults to "text"
-        embeddingKey: "embedding", // The name of the collection field containing the embedded text. Defaults to "embedding"
-    })
-
-}
+// // }
 //     // const assignedIds = await vectorStore.addDocuments([
 //     //     { pageContent: "upsertable", metadata: {} },
 //     // ]);
